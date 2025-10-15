63658f79c3164840340c82a9adcf4c6950406ec5
Nathan-J-22450784 <22450784@students.latrobe.edu.au>
2025-10-15 17:37:42 +1100

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import JsonResponse
-
-def health(_):
-    return JsonResponse({"status": "ok"})
+from api.views import health 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
